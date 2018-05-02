e8ff1b4f2a1ff2e94b27ae304f3d4318b0e31a89
Your Name <you@example.com>
2018-05-02 09:38:36 -0500

    squashed == === errors

--- src/Containers/RichTextEditor/RichTextEditor.js
+++ src/Containers/RichTextEditor/RichTextEditor.js
@@ -1,11 +1,10 @@
-import { Editor } from 'slate-react';
-import { Value } from 'slate';
-import Html from 'slate-html-serializer';
-import React from 'react';
-import { isKeyHotkey } from 'is-hotkey';
-import {  Row, Col,Button } from 'reactstrap';
-import initialValue from './value.json';
-
+import { Editor } from "slate-react";
+import { Value } from "slate";
+import Html from "slate-html-serializer";
+import React from "react";
+import { isKeyHotkey } from "is-hotkey";
+import { Row, Col, Button } from "reactstrap";
+import initialValue from "./value.json";
 
 /**
  * Define the default node type.
@@ -13,7 +12,7 @@ import initialValue from './value.json';
  * @type {String}
  */
 
-const DEFAULT_NODE = 'paragraph';
+const DEFAULT_NODE = "paragraph";
 
 /**
  * Define hotkey matchers.
@@ -21,91 +20,90 @@ const DEFAULT_NODE = 'paragraph';
  * @type {Function}
  */
 
-const isBoldHotkey = isKeyHotkey('mod+b');
-const isItalicHotkey = isKeyHotkey('mod+i');
-const isUnderlinedHotkey = isKeyHotkey('mod+u');
-const isCodeHotkey = isKeyHotkey('mod+`');
+const isBoldHotkey = isKeyHotkey("mod+b");
+const isItalicHotkey = isKeyHotkey("mod+i");
+const isUnderlinedHotkey = isKeyHotkey("mod+u");
+const isCodeHotkey = isKeyHotkey("mod+`");
 
 const BLOCK_TAGS = {
-  blockquote: 'block-quote',
-    p: 'paragraph',
-    li:'list-item',
-    ol:'numbered-list',
-    ul:'bulleted-list'
-}
+  blockquote: "block-quote",
+  p: "paragraph",
+  li: "list-item",
+  ol: "numbered-list",
+  ul: "bulleted-list"
+};
 // Add a dictionary of mark tags.
 const MARK_TAGS = {
-    em: 'italic',
-    strong: 'bold',
-    u: 'underline',
-    pre: 'code'
-}
+  em: "italic",
+  strong: "bold",
+  u: "underline",
+  pre: "code"
+};
 const rules = [
   {
     deserialize(el, next) {
-        const type = BLOCK_TAGS[el.tagName.toLowerCase()];
+      const type = BLOCK_TAGS[el.tagName.toLowerCase()];
       if (type) {
         return {
-          object: 'block',
+          object: "block",
           type: type,
           nodes: next(el.childNodes)
         };
       }
     },
     serialize(obj, children) {
-      if (obj.object === 'block') {
+      if (obj.object === "block") {
         switch (obj.type) {
-          case 'paragraph':
+          case "paragraph":
             return <p>{children}</p>;
-          case 'block-quote':
+          case "block-quote":
             return <blockquote>{children}</blockquote>;
-        case 'bulleted-list':
+          case "bulleted-list":
             return <ul>{children}</ul>;
-        case 'numbered-list':
+          case "numbered-list":
             return <ol>{children}</ol>;
-        case 'list-item':
+          case "list-item":
             return <li>{children}</li>;
-        default:
+          default:
             console.log("should never happen richtexteditor line 69");
         }
       }
-    },
+    }
   },
   // Add a new rule that handles marks...
   {
     deserialize(el, next) {
-        const type = MARK_TAGS[el.tagName.toLowerCase()];
+      const type = MARK_TAGS[el.tagName.toLowerCase()];
       if (type) {
         return {
-          object: 'mark',
+          object: "mark",
           type: type,
-          nodes: next(el.childNodes),
+          nodes: next(el.childNodes)
         };
       }
     },
     serialize(obj, children) {
-      if (obj.object === 'mark') {
+      if (obj.object === "mark") {
         switch (obj.type) {
-          case 'bold':
+          case "bold":
             return <strong>{children}</strong>;
-          case 'italic':
+          case "italic":
             return <em>{children}</em>;
-          case 'underline':
+          case "underline":
             return <u>{children}</u>;
-        case 'code':
+          case "code":
             return (
-                <pre>
-                  <code>{children}</code>
-                </pre>
+              <pre>
+                <code>{children}</code>
+              </pre>
             );
-        default:
+          default:
             console.log("should never happen rich text line 102");
-
         }
       }
-    },
-  },
-]
+    }
+  }
+];
 const html = new Html({ rules });
 /**
  * The rich text example.
@@ -120,27 +118,30 @@ class RichTextExample extends React.Component {
    */
 
   state = {
-      value: Value.fromJSON(initialValue),
-      posts:this.props.posts
+    value: Value.fromJSON(initialValue),
+    posts: this.props.posts
+  };
+  componentWillMount() {
+    const cookie = this.getCookie("token");
+    if (cookie) {
+      this.setState({ token: cookie });
+    }
   }
-    componentWillMount(){
-        const cookie = this.getCookie("token");
-        if (cookie){
-            this.setState({token:cookie});
-        }
-        
-    }
-    getCookie(name) {
-        var value = "; " + document.cookie;
-        var parts = value.split("; " + name + "=");
-        if (parts.length === 2) return parts.pop().split(";").shift();
-    }
+  getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length === 2)
+      return parts
+        .pop()
+        .split(";")
+        .shift();
+  }
 
-    componentDidMount(){
-        if(this.props.description){
-            this.setState({value: Value.fromJSON(this.props.description)});
-        }
+  componentDidMount() {
+    if (this.props.description) {
+      this.setState({ value: Value.fromJSON(this.props.description) });
     }
+  }
   /**
    * Check if the current selection has a mark with `type` in it.
    *
@@ -149,9 +150,9 @@ class RichTextExample extends React.Component {
    */
 
   hasMark = type => {
-      const { value } = this.state;
-      return value.activeMarks.some(mark => mark.type == type);
-  }
+    const { value } = this.state;
+    return value.activeMarks.some(mark => mark.type === type);
+  };
 
   /**
    * Check if the any of the currently selected blocks are of `type`.
@@ -161,9 +162,9 @@ class RichTextExample extends React.Component {
    */
 
   hasBlock = type => {
-      const { value } = this.state;
-      return value.blocks.some(node => node.type == type);
-  }
+    const { value } = this.state;
+    return value.blocks.some(node => node.type === type);
+  };
 
   /**
    * On change, save the new `value`.
@@ -172,8 +173,8 @@ class RichTextExample extends React.Component {
    */
 
   onChange = ({ value }) => {
-      this.setState({ value });
-  }
+    this.setState({ value });
+  };
 
   /**
    * On key down, if it's a formatting command toggle a mark.
@@ -184,24 +185,24 @@ class RichTextExample extends React.Component {
    */
 
   onKeyDown = (event, change) => {
-      let mark;
+    let mark;
 
     if (isBoldHotkey(event)) {
-        mark = 'bold';
+      mark = "bold";
     } else if (isItalicHotkey(event)) {
-        mark = 'italic';
+      mark = "italic";
     } else if (isUnderlinedHotkey(event)) {
-        mark = 'underlined';
+      mark = "underlined";
     } else if (isCodeHotkey(event)) {
-        mark = 'code';
+      mark = "code";
     } else {
-        return;
+      return;
     }
 
-      event.preventDefault();
-      change.toggleMark(mark);
-      return true;
-  }
+    event.preventDefault();
+    change.toggleMark(mark);
+    return true;
+  };
 
   /**
    * When a mark button is clicked, toggle the current mark.
@@ -211,11 +212,11 @@ class RichTextExample extends React.Component {
    */
 
   onClickMark = (event, type) => {
-      event.preventDefault();
-      const { value } = this.state;
-      const change = value.change().toggleMark(type);
-      this.onChange(change);
-  }
+    event.preventDefault();
+    const { value } = this.state;
+    const change = value.change().toggleMark(type);
+    this.onChange(change);
+  };
 
   /**
    * When a block button is clicked, toggle the block type.
@@ -225,56 +226,55 @@ class RichTextExample extends React.Component {
    */
 
   onClickBlock = (event, type) => {
-      event.preventDefault();
-      const { value } = this.state;
-      const change = value.change();
-      const { document } = value;
+    event.preventDefault();
+    const { value } = this.state;
+    const change = value.change();
+    const { document } = value;
 
     // Handle everything but list buttons.
-    if (type != 'bulleted-list' && type != 'numbered-list') {
-        const isActive = this.hasBlock(type);
-        const isList = this.hasBlock('list-item');
+    if (type !== "bulleted-list" && type !== "numbered-list") {
+      const isActive = this.hasBlock(type);
+      const isList = this.hasBlock("list-item");
 
       if (isList) {
         change
           .setBlocks(isActive ? DEFAULT_NODE : type)
-          .unwrapBlock('bulleted-list')
-              .unwrapBlock('numbered-list');
+          .unwrapBlock("bulleted-list")
+          .unwrapBlock("numbered-list");
       } else {
-          change.setBlocks(isActive ? DEFAULT_NODE : type);
+        change.setBlocks(isActive ? DEFAULT_NODE : type);
       }
     } else {
       // Handle the extra wrapping required for list buttons.
-        const isList = this.hasBlock('list-item');
+      const isList = this.hasBlock("list-item");
       const isType = value.blocks.some(block => {
-          return !!document.getClosest(block.key, parent => parent.type == type);
-      })
+        return !!document.getClosest(block.key, parent => parent.type === type);
+      });
 
       if (isList && isType) {
         change
           .setBlocks(DEFAULT_NODE)
-          .unwrapBlock('bulleted-list')
-              .unwrapBlock('numbered-list');
+          .unwrapBlock("bulleted-list")
+          .unwrapBlock("numbered-list");
       } else if (isList) {
         change
           .unwrapBlock(
-            type == 'bulleted-list' ? 'numbered-list' : 'bulleted-list'
+            type === "bulleted-list" ? "numbered-list" : "bulleted-list"
           )
-              .wrapBlock(type);
+          .wrapBlock(type);
       } else {
-          change.setBlocks('list-item').wrapBlock(type);
+        change.setBlocks("list-item").wrapBlock(type);
       }
     }
 
-      this.onChange(change);
-  }
+    this.onChange(change);
+  };
 
-    postContent (value){
-        const string = html.serialize(value);
-        console.log(string);
-        return string;
-    }
-  
+  postContent(value) {
+    const string = html.serialize(value);
+    console.log(string);
+    return string;
+  }
 
   /**
    * Render.
@@ -283,29 +283,45 @@ class RichTextExample extends React.Component {
    */
 
   render() {
-      let deleteButton =null;
-      if(this.props.deleteBool){
-          deleteButton= <Button onClick={()=>{
-              this.props.del(this.props.destination,this.props.id);
-              console.log(this.props.id);
-          }}  color="danger">Delete</Button>;
-
-      }
+    let deleteButton = null;
+    if (this.props.deleteBool) {
+      deleteButton = (
+        <Button
+          onClick={() => {
+            this.props.del(this.props.destination, this.props.id);
+            console.log(this.props.id);
+          }}
+          color="danger"
+        >
+          Delete
+        </Button>
+      );
+    }
     return (
       <div>
         {this.renderToolbar()}
-        <br/>
+        <br />
         {this.renderEditor()}
-        <br/>
-        {deleteButton}{' '}
-        <Button color="primary" onClick={() => {
-              this.props.post(this.state.value,this.postContent(this.state.value), this.props.destination);
-              if(this.props.quick){
-                  this.setState({value: Value.fromJSON(initialValue)});
-              }
-          }}>Submit</Button>{''}
+        <br />
+        {deleteButton}{" "}
+        <Button
+          color="primary"
+          onClick={() => {
+            this.props.post(
+              this.state.value,
+              this.postContent(this.state.value),
+              this.props.destination
+            );
+            if (this.props.quick) {
+              this.setState({ value: Value.fromJSON(initialValue) });
+            }
+          }}
+        >
+          Submit
+        </Button>
+        {""}
       </div>
-    )
+    );
   }
 
   /**
@@ -316,20 +332,34 @@ class RichTextExample extends React.Component {
 
   renderToolbar = () => {
     return (
-        <Row >
-          <Col xs="12" className="d-flex justify-content-around ">
-
-            {this.renderMarkButton('bold', <i className="fas fa-bold"></i>)}
-        {this.renderMarkButton('italic', <i className="fas fa-italic"></i>)}
-        {this.renderMarkButton('underlined', <i className="fas fa-underline"></i>)}
-        {this.renderBlockButton('heading-one', <i className="fas fa-heading"></i>)}
-        {this.renderBlockButton('block-quote', <i className="fas fa-quote-left"></i>)}
-        {this.renderBlockButton('numbered-list', <i className="fas fa-list-ol"></i>)}
-        {this.renderBlockButton('bulleted-list', <i className="fas fa-list-ul"></i>)}
-          </Col>
-        </Row>
-    )
-  }
+      <Row>
+        <Col xs="12" className="d-flex justify-content-around ">
+          {this.renderMarkButton("bold", <i className="fas fa-bold" />)}
+          {this.renderMarkButton("italic", <i className="fas fa-italic" />)}
+          {this.renderMarkButton(
+            "underlined",
+            <i className="fas fa-underline" />
+          )}
+          {this.renderBlockButton(
+            "heading-one",
+            <i className="fas fa-heading" />
+          )}
+          {this.renderBlockButton(
+            "block-quote",
+            <i className="fas fa-quote-left" />
+          )}
+          {this.renderBlockButton(
+            "numbered-list",
+            <i className="fas fa-list-ol" />
+          )}
+          {this.renderBlockButton(
+            "bulleted-list",
+            <i className="fas fa-list-ul" />
+          )}
+        </Col>
+      </Row>
+    );
+  };
 
   /**
    * Render a mark-toggling toolbar button.
@@ -341,20 +371,24 @@ class RichTextExample extends React.Component {
    */
 
   renderMarkButton = (type, icon) => {
-      const isActive = this.hasMark(type);
-      let bg ="";
-      if (isActive === true){
-          bg ="bg-primary";
-      }
-      const onMouseDown = event => this.onClickMark(event, type);
+    const isActive = this.hasMark(type);
+    let bg = "";
+    if (isActive === true) {
+      bg = "bg-primary";
+    }
+    const onMouseDown = event => this.onClickMark(event, type);
 
     return (
       // eslint-disable-next-line react/jsx-no-bind
-        <Col className={"control " + bg} onMouseDown={onMouseDown} data-active={isActive}>
-        <span >{icon}</span>
+      <Col
+        className={"control " + bg}
+        onMouseDown={onMouseDown}
+        data-active={isActive}
+      >
+        <span>{icon}</span>
       </Col>
-    )
-  }
+    );
+  };
 
   /**
    * Render a block-toggling toolbar button.
@@ -365,20 +399,24 @@ class RichTextExample extends React.Component {
    */
 
   renderBlockButton = (type, icon) => {
-      const isActive = this.hasBlock(type);
-      let bg ="";
-      if (isActive === true){
-          bg ="bg-primary";
-      }
-      const onMouseDown = event => this.onClickBlock(event, type);
+    const isActive = this.hasBlock(type);
+    let bg = "";
+    if (isActive === true) {
+      bg = "bg-primary";
+    }
+    const onMouseDown = event => this.onClickBlock(event, type);
 
     return (
       // eslint-disable-next-line react/jsx-no-bind
-        <Col className={"control " + bg} onMouseDown={onMouseDown} data-active={isActive}>
-        <span >{icon}</span>
+      <Col
+        className={"control " + bg}
+        onMouseDown={onMouseDown}
+        data-active={isActive}
+      >
+        <span>{icon}</span>
       </Col>
-    )
-  }
+    );
+  };
 
   /**
    * Render the Slate editor.
@@ -399,8 +437,8 @@ class RichTextExample extends React.Component {
           spellCheck
         />
       </Col>
-    )
-  }
+    );
+  };
 
   /**
    * Render a Slate node.
@@ -410,22 +448,22 @@ class RichTextExample extends React.Component {
    */
 
   renderNode = props => {
-      const { attributes, children, node } = props;
+    const { attributes, children, node } = props;
     switch (node.type) {
-      case 'block-quote':
+      case "block-quote":
         return <blockquote {...attributes}>{children}</blockquote>;
-      case 'bulleted-list':
+      case "bulleted-list":
         return <ul {...attributes}>{children}</ul>;
-      case 'heading-one':
+      case "heading-one":
         return <h1 {...attributes}>{children}</h1>;
-      case 'heading-two':
+      case "heading-two":
         return <h2 {...attributes}>{children}</h2>;
-      case 'list-item':
+      case "list-item":
         return <li {...attributes}>{children}</li>;
-      case 'numbered-list':
+      case "numbered-list":
         return <ol {...attributes}>{children}</ol>;
     }
-  }
+  };
 
   /**
    * Render a Slate mark.
@@ -435,18 +473,18 @@ class RichTextExample extends React.Component {
    */
 
   renderMark = props => {
-      const { children, mark } = props;
+    const { children, mark } = props;
     switch (mark.type) {
-      case 'bold':
+      case "bold":
         return <strong>{children}</strong>;
-      case 'code':
+      case "code":
         return <code>{children}</code>;
-      case 'italic':
+      case "italic":
         return <em>{children}</em>;
-      case 'underlined':
+      case "underlined":
         return <u>{children}</u>;
     }
-  }
+  };
 }
 
 /**
