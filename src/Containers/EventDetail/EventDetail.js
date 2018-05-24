097feccea691f3769df10513221309ec09354c11
Your Name <you@example.com>
2018-05-24 00:16:22 -0500

    finished events

--- src/Containers/EventDetail/EventDetail.js
+++ src/Containers/EventDetail/EventDetail.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+
 const baseAPIURL = "https://api.bvzzdesign.com/lonehen";
 
 export default class EventDetail extends React.Component {
@@ -15,6 +18,52 @@ export default class EventDetail extends React.Component {
       .then(data => this.setState({ events: data }));
   }
   render() {
-    return <div>{this.state.events.event_title}</div>;
+    return (
+      <Container>
+        <Row>
+          <Col sm="12" className="text-center my-5 ">
+            <h2>{this.state.events.event_title}</h2>
+          </Col>
+          <Col md="4">
+            <Link className="dark" to="/events">
+              <i className="fas fa-arrow-left" /> Back to events
+            </Link>
+          </Col>
+          <Col md="8">
+            <h4>
+              {" "}
+              {new Date(this.state.events.event_start_date).toLocaleString(
+                "en-us",
+                {
+                  weekday: "long",
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric"
+                }
+              )}{" "}
+              -{" "}
+              {new Date(this.state.events.event_end_date).toLocaleString(
+                "en-us",
+                {
+                  weekday: "long",
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric"
+                }
+              )}{" "}
+            </h4>
+            <p>
+              {this.state.events.event_start_time} -{" "}
+              {this.state.events.event_end_time}
+            </p>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: this.state.events.event_details
+              }}
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
